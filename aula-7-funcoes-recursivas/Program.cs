f5766c34a1ec277f6f66e522074c106e745ed939
nicollas <nicollaslopesjf@gmail.com>
2018-08-30 20:41:56 -0300

    file

---
+++ aula-7-funcoes-recursivas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_7_funcoes_recursivas
+{
+    class Program
+    {
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"C:\arquivos\arq" + numeroArquivo +".txt";
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+
+            string arquivoComCaminho2 = @"C:\arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+        }
+
+          
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+            Console.Read();
+        }
+    }
+}
+  
